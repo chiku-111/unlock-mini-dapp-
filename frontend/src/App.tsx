9e1aa58ede269cb0a9738378152c498aa1f23ada
chiku <267832241+chiku-111@users.noreply.github.com>
2026-04-29 20:13:30 +0800

    Add mock wallet state to React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,49 @@
+//useState状态变化 locked/unlocked
+import { useState } from 'react'
+import './App.css'
+
+type AccessState = "locked" | "unlocked";
+
+
+// 创建一个页面状态 accessState: 初始值是 "locked"
+// setAccessState 用来修改这个状态，修改后 React 会自动刷新页面。
+function App() {
+const [accessState, setAccessState] = useState<AccessState>("locked");
+const [walletAddress, setWalletAdddress]=useState<string | null>(null);
+
+function handleConnectWallet(){
+  setWalletAdddress("0x1234...abcd");
+}
+
+function handleCheckMembership() {
+  setAccessState("unlocked");
+}
+
+  return (
+    <main>
+      <h1>Unlock Mini DApp</h1>
+
+      {/*If left is null - not connected, else left*/}
+      <p>Wallet: {walletAddress ?? "not connected"}</p> 
+
+      <p>Current status: {accessState}</p>
+
+      <button type="button">Connect Wallet</button>
+
+      <button type="button" onClick={handleCheckMembership}>
+        Check Membership
+      </button>
+
+      <button type="button" onClick={handleConnectWallet}>
+        Connect Wallet
+      </button>
+    </main>
+  )
+}
+
+export default App
+
+/*<button type="button" onClick = {() => setAccessState("unlocked")}>
+        Check Membership
+      </button> */
+  
